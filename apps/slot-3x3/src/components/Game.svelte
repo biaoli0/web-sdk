9178f7ffdffa5fc946c4a645ce212954996dd62c
billli0 <lbit123@outlook.com>
2026-05-27 16:50:52 +1000

    Add game info

--- apps/slot-3x3/src/components/Game.svelte
+++ apps/slot-3x3/src/components/Game.svelte
@@ -1,6 +1,9 @@
 <script lang="ts">
 	import { App, Text } from 'pixi-svelte';
 	import { MainContainer } from 'components-layout';
+	import { EnableHotkey } from 'components-shared';
+	import { UI, UiGameName } from 'components-ui-pixi';
+	import { GameVersion, Modals } from 'components-ui-html';
 
 	import { getContext } from '../game/context';
 	import Board from './Board.svelte';
@@ -10,11 +13,32 @@
 </script>
 
 <App>
+	<EnableHotkey />
 	<EnableGameActor />
+
 	{#if context.stateApp.loaded}
 		<MainContainer>
 			<Board />
 		</MainContainer>
+
+		<UI>
+			{#snippet gameName()}
+				<UiGameName name="3X3 SLOT" />
+			{/snippet}
+			{#snippet logo()}
+				<Text
+					anchor={{ x: 1, y: 0 }}
+					text="ADD YOUR LOGO"
+					style={{
+						fontFamily: 'proxima-nova',
+						fontSize: 24,
+						fontWeight: '600',
+						lineHeight: 32,
+						fill: 0xffffff,
+					}}
+				/>
+			{/snippet}
+		</UI>
 	{:else}
 		<Text
 			x={40}
@@ -29,3 +53,9 @@
 		/>
 	{/if}
 </App>
+
+<Modals>
+	{#snippet version()}
+		<GameVersion version="0.0.0" />
+	{/snippet}
+</Modals>
